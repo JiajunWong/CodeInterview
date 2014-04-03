6a3f3ef6147b53dbd3ff7f18582530e7abec66d1
Jiajun Wang <jwang@stumbleupon.com>
2014-04-03 14:33:12 -0700

    ReverseInteger

---
+++ src/InterviewCode/ReverseInteger.java
@@ -0,0 +1,44 @@
+package InterviewCode;
+
+public class ReverseInteger
+{
+    public static int reverse(int x)
+    {
+        boolean flag = false;
+        if (x < 0)
+        {
+            x = -x;
+            flag = true;
+        }
+        
+        int offset = 0;
+        int result = 0;
+        int left = x;
+        while (left >= 10)
+        {
+            offset = left % 10;
+            left = left / 10;
+            result = offset + result * 10;
+        }
+        offset = left % 10;
+        result = result * 10 + offset;
+        
+        if (flag)
+        {
+            result = -result;
+        }
+        
+        return result;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        int i = -54321;
+        int result = reverse(i);
+        System.out.println(result);
+    }
+
+}
