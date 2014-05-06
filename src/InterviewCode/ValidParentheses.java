d589ab117490e2951811059923df805da6b5c288
jwang <jwang@stumbleupon.com>
2014-05-05 23:25:46 -0700

    ValidParentheses

---
+++ src/InterviewCode/ValidParentheses.java
@@ -0,0 +1,48 @@
+package InterviewCode;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses
+{
+    public boolean isValid(String s)
+    {
+        char[] charArray = s.toCharArray();
+
+        HashMap<Character, Character> hashMap = new HashMap<Character, Character>();
+        hashMap.put('(', ')');
+        hashMap.put('[', ']');
+        hashMap.put('{', '}');
+
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : charArray)
+        {
+            if (hashMap.keySet().contains(c))
+            {
+                stack.push(c);
+            }
+            else if (hashMap.values().contains(c))
+            {
+                if (!stack.isEmpty() && hashMap.get(stack.peek()) == c)
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
