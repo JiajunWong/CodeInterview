f1ba3fc468440f71407d0070cae44888f0c279d9
Jiajun Wang <jwang@stumbleupon.com>
2014-05-19 15:27:07 -0700

    LongestValidParentheses

---
+++ src/InterviewCode/LongestValidParentheses.java
@@ -0,0 +1,44 @@
+package InterviewCode;
+
+import java.util.Arrays;
+
+public class LongestValidParentheses
+{
+    public int longestValidParentheses(String s)
+    {
+        int result = 0;
+        int length = s.length();
+        int d[] = new int[length];
+
+        Arrays.fill(d, 0);
+
+        for (int i = length - 2; i >= 0; i--)
+        {
+            if (s.charAt(i) == '(')
+            {
+                int j = i + 1 + d[i + 1];
+                if (j <= length - 1 && s.charAt(j) == ')')
+                {
+                    d[i] = d[i + 1] + 2;
+                    if (j + 1 <= length - 1)
+                    {
+                        d[i] += d[j + 1];
+                    }
+                }
+                result = Math.max(result, d[i]);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
