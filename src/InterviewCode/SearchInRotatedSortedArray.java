b4b9bd7ac9a47cbccaecfd9a280c3709219078ec
Jiajun Wang <jwang@stumbleupon.com>
2014-05-19 17:11:04 -0700

    SearchInRotatedSortedArray

---
+++ src/InterviewCode/SearchInRotatedSortedArray.java
@@ -0,0 +1,53 @@
+package InterviewCode;
+
+public class SearchInRotatedSortedArray
+{
+    public int search(int[] A, int target)
+    {
+        int left = 0;
+        int right = A.length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2; //good to know this can avoid overflow;
+            if (A[mid] == target)
+                return mid;
+
+            if (A[left] <= A[mid]) //left is sorted;
+            {
+                if (A[left] <= target && target < A[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else
+            //right is sorted;
+            {
+                if (A[mid] < target && target <= A[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
