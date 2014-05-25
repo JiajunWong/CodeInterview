8e4cde0119e0d70316dda20b6b5c6abdedd30363
jwang <jwang@stumbleupon.com>
2014-05-25 15:09:10 -0700

    JumpGameII

---
+++ src/InterviewCode/JumpGameII.java
@@ -0,0 +1,32 @@
+package InterviewCode;
+
+public class JumpGameII
+{
+    public int jump(int[] A)
+    {
+        int result = 0;
+        int last = 0;
+        int curr = 0;
+
+        for (int i = 0; i < A.length; i++)
+        {
+            if (i > last)
+            {
+                last = curr;
+                result++;
+            }
+            curr = Math.max(curr, i + A[i]);
+        }
+        return result;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
