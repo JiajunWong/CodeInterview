d6317f72e1bb3c0255924d81c14df84de9c73ee8
jwang <jwang@stumbleupon.com>
2014-06-09 23:36:03 -0700

    JumpGame

---
+++ src/InterviewCode/JumpGame.java
@@ -0,0 +1,17 @@
+package InterviewCode;
+
+public class JumpGame
+{
+    public boolean canJump(int[] A)
+    {
+        int maxCover = 0;
+        for (int i = 0; i <= maxCover && i < A.length; i++)
+        {
+            if (A[i] + i > maxCover)
+                maxCover = A[i] + i;
+            if (maxCover >= A.length - 1)
+                return true;
+        }
+        return false;
+    }
+}
