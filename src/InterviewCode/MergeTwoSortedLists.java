69e3e47e372c879f1252b4d2d81253da225b6ae5
Jiajun Wang <jwang@stumbleupon.com>
2014-06-12 11:59:09 -0700

    MergeTwoSortedLists

---
+++ src/InterviewCode/MergeTwoSortedLists.java
@@ -0,0 +1,27 @@
+package InterviewCode;
+
+import Util.ListNode;
+
+public class MergeTwoSortedLists
+{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+    {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        ListNode result = null;
+        if (l1.val > l2.val)
+        {
+            result = l2;
+            result.next = mergeTwoLists(l1, l2.next);
+        }
+        else
+        {
+            result = l1;
+            result.next = mergeTwoLists(l1.next, l2);
+        }
+
+        return result;
+    }
+}
