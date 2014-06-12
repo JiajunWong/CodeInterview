056d13caa179fd7cce10f02137e1e3bac1ce5320
Jiajun Wang <jwang@stumbleupon.com>
2014-06-12 11:36:37 -0700

    uniquePaths

---
+++ src/InterviewCode/UniquePaths.java
@@ -0,0 +1,11 @@
+package InterviewCode;
+
+public class UniquePaths
+{
+    public int uniquePaths(int m, int n)
+    {
+        if (n == 1 || m == 1)
+            return 1;
+        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+}
