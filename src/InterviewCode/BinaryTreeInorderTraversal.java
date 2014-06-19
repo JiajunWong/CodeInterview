d00d55a15f9da9d4d7da8b047d2ebde074a54e08
jwang <jwang@stumbleupon.com>
2014-06-18 22:16:51 -0700

    BinaryTreeInorderTraversal

---
+++ src/InterviewCode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,27 @@
+package InterviewCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Util.TreeNode;
+
+public class BinaryTreeInorderTraversal
+{
+    public List<Integer> inorderTraversal(TreeNode root)
+    {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        rec(root, result);
+        return result;
+    }
+
+    private void rec(TreeNode root, ArrayList<Integer> result)
+    {
+        if (root == null)
+        {
+            return;
+        }
+        rec(root.left, result);
+        result.add(root.val);
+        rec(root.right, result);
+    }
+}
