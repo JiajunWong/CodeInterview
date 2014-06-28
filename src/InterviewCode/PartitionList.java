79753a1a1f2bde1b48736646002d0ac374bc4999
jwang <jwang@stumbleupon.com>
2014-06-28 13:53:41 -0700

    CloneGraph

--- src/InterviewCode/PartitionList.java
+++ src/InterviewCode/PartitionList.java
@@ -1,6 +1,5 @@
 package InterviewCode;
 
-import Util.ListNode;
 
 public class PartitionList
 {
---
+++ src/Util/UndirectedGraphNode.java
@@ -0,0 +1,15 @@
+package Util;
+
+import java.util.ArrayList;
+
+public class UndirectedGraphNode
+{
+    public int label;
+    public ArrayList<UndirectedGraphNode> neighbors;
+
+    public UndirectedGraphNode(int x)
+    {
+        this.label = x;
+        neighbors = new ArrayList<UndirectedGraphNode>();
+    }
+}
