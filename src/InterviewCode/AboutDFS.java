2a225222a614cb8f7573c633ec2fe0d0850c7ec2
Jiajun Wang <jwang@stumbleupon.com>
2014-07-08 17:23:19 -0700

    solveSudoku

--- src/InterviewCode/AboutDFS.java
+++ src/InterviewCode/AboutDFS.java
@@ -1,6 +1,7 @@
 package InterviewCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -75,6 +76,200 @@ public class AboutDFS
     //Sudoku Solver
     public void solveSudoku(char[][] board)
     {
+        dfs(board, 0, 0);
+    }
 
+    private boolean dfs(char[][] board, int i, int j)
+    {
+        if (j >= 9)
+        {
+            return dfs(board, i + 1, 0);
+        }
+        if (i == 9)
+        {
+            return true;
+        }
+        if (board[i][j] == '.')
+        {
+            for (int k = 1; k <= 9; k++)
+            {
+                board[i][j] = (char) (k + '0');
+                if (isValid(board, i, j))
+                {
+                    if (dfs(board, i, j + 1))
+                    {
+                        return true;
+                    }
+                }
+                board[i][j] = '.';
+            }
+        }
+        else
+        {
+            return dfs(board, i, j + 1);
+        }
+        return false;
+    }
+
+    private boolean isValid(char[][] board, int i, int j)
+    {
+        for (int k = 0; k < 9; k++)
+        {
+            if (k != j && board[i][k] == board[i][j])
+                return false;
+        }
+        for (int k = 0; k < 9; k++)
+        {
+            if (k != i && board[k][j] == board[i][j])
+                return false;
+        }
+        for (int row = i / 3 * 3; row < i / 3 * 3 + 3; row++)
+        {
+            for (int col = j / 3 * 3; col < j / 3 * 3 + 3; col++)
+            {
+                if ((row != i || col != j) && board[row][col] == board[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    //N-Queens
+    public List<String[]> solveNQueens(int n)
+    {
+        ArrayList<String[]> result = new ArrayList<String[]>();
+        if (n <= 0)
+        {
+            return result;
+        }
+        String[] rows = new String[n];
+        dfs(0, n, rows, result);
+        return result;
+    }
+
+    private void dfs(int row, int n, String[] rows, ArrayList<String[]> result)
+    {
+        if (row >= n)
+        {
+            result.add(rows.clone());
+            return;
+        }
+        for (int col = 0; col < n; col++)
+        {
+            if (!isValid(col, row, rows))
+            {
+                continue;
+            }
+
+            char[] chars = new char[n];
+            Arrays.fill(chars, '.');
+            chars[col] = 'Q';
+
+            rows[row] = String.valueOf(chars);
+            dfs(row + 1, n, rows, result);
+        }
+    }
+
+    private boolean isValid(int col, int row, String[] rows)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            int indexQ = rows[i].indexOf('Q');
+            if (indexQ == col)
+            {
+                return false;
+            }
+
+            int rowDis = Math.abs(row - i);
+            int colDis = Math.abs(col - indexQ);
+            if (rowDis == colDis)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //N-Queens II
+    public int totalNQueens(int n)
+    {
+        int[] result = new int[1];
+        if (n <= 0)
+        {
+            return result[0];
+        }
+        String[] rows = new String[n];
+        dfs(0, n, rows, result);
+        return result[0];
+    }
+
+    private void dfs(int row, int n, String[] rows, int[] result)
+    {
+        if (row >= n)
+        {
+            result[0] += 1;
+            return;
+        }
+        for (int col = 0; col < n; col++)
+        {
+            if (!isValid(col, row, rows))
+            {
+                continue;
+            }
+
+            char[] chars = new char[n];
+            Arrays.fill(chars, '.');
+            chars[col] = 'Q';
+
+            rows[row] = String.valueOf(chars);
+            dfs(row + 1, n, rows, result);
+        }
+    }
+
+    //Word Search
+    public boolean exist(char[][] board, String word)
+    {
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board[0].length; j++)
+            {
+                if (dfs(board, word, 0, i, j))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int index, int i, int j)
+    {
+        if (index == word.length() - 1 && word.charAt(word.length() - 1) == board[i][j])
+        {
+            return true;
+        }
+        if (word.charAt(index) != board[i][j])
+        {
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '.';
+        boolean b1 = false, b2 = false, b3 = false, b4 = false;
+        if (i - 1 >= 0 && board[i - 1][j] != '.')
+        {
+            b1 = dfs(board, word, index + 1, i - 1, j);
+        }
+        if (!b1 && j - 1 >= 0 && board[i][j - 1] != '.')
+        {
+            b2 = dfs(board, word, index + 1, i, j - 1);
+        }
+        if (!b1 && !b2 && i + 1 < board.length && board[i + 1][j] != '.')
+        {
+            b3 = dfs(board, word, index + 1, i + 1, j);
+        }
+        if (!b1 && !b2 && b3 && j + 1 < board[0].length && board[i][j + 1] != '.')
+        {
+            b4 = dfs(board, word, index + 1, i, j + 1);
+        }
+        board[i][j] = temp;
+        return b1 || b2 || b3 || b4;
     }
 }
