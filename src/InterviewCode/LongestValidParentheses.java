520ce1e1fc49b2eac39bf84e25542784049775ea
jwang <jwang@stumbleupon.com>
2014-07-17 23:06:12 -0700

    LongestValidParentheses

--- src/InterviewCode/LongestValidParentheses.java
+++ src/InterviewCode/LongestValidParentheses.java
@@ -2,6 +2,7 @@ package InterviewCode;
 
 import java.util.Arrays;
 
+//http://leetcodenotes.wordpress.com/2013/10/19/leetcode-longest-valid-parentheses-%E8%BF%99%E7%A7%8D%E6%8B%AC%E5%8F%B7%E7%BB%84%E5%90%88%EF%BC%8C%E6%9C%80%E9%95%BF%E7%9A%84valid%E6%8B%AC%E5%8F%B7%E7%BB%84%E5%90%88%E6%9C%89%E5%A4%9A/
 public class LongestValidParentheses
 {
     public int longestValidParentheses(String s)
