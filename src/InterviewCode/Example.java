fa768aa1b17b3579b7d6fd59f54ce3a1a8fa69e1
Jiajun Wang <jwang@stumbleupon.com>
2014-07-22 16:47:45 -0700

    ClimbingStairs

---
+++ src/InterviewCode/Example.java
@@ -0,0 +1,12 @@
+package InterviewCode;
+
+import java.util.Arrays;
+
+public class Example
+{
+    public static void main(String[] args)
+    {
+        int[] dp = new int[2];
+        Arrays.fill(dp, 0);
+    }
+}
