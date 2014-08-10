0cc4241c0ed0393d7e20709183b7326053bcd183
jwang <jwang@stumbleupon.com>
2014-08-10 15:19:13 -0700

    LargestRectangleInHistogram

--- src/InterviewCode/LargestRectangleInHistogram.java
+++ src/InterviewCode/LargestRectangleInHistogram.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 
 public class LargestRectangleInHistogram
 {
-    public int largestRectangleArea2(int[] height)
+    public int largestRectangleArea(int[] height)
     {
         Stack<Integer> stack = new Stack<Integer>();
         int i = 0;
