e0a99639b5b54d5be6845ce57892cac2c5a838de
Jiajun Wang <jwang@stumbleupon.com>
2014-08-11 10:56:58 -0700

    sortColors

--- src/InterviewCode/SortColors.java
+++ src/InterviewCode/SortColors.java
@@ -4,20 +4,6 @@ public class SortColors
 {
     public void sortColors(int[] A)
     {
-<<<<<<< HEAD
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        int N = A.length;
-        int low = 0, it = 0, hi = N - 1;
-        while (it <= hi)
-        {
-            if (A[it] == 0)
-            {
-                swap(A, low++, it++);
-            }
-            else if (A[it] == 2)
-            {
-                swap(A, it, hi--);
-=======
         int len = A.length;
         int start = 0, it = 0, end = len - 1;
         while (it <= end)
@@ -29,7 +15,6 @@ public class SortColors
             else if (A[it] == 2)
             {
                 swap(A, it, end--);
->>>>>>> FETCH_HEAD
             }
             else
             {
@@ -38,18 +23,10 @@ public class SortColors
         }
     }
 
-<<<<<<< HEAD
-    private void swap(int[] A, int i, int j)
-    {
-        int tem = A[i];
-        A[i] = A[j];
-        A[j] = tem;
-=======
     private void swap(int[] A, int a, int b)
     {
         int t = A[a];
         A[a] = A[b];
         A[b] = t;
->>>>>>> FETCH_HEAD
     }
 }
