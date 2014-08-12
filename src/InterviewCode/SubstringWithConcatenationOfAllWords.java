686b4e2f239d2650cb60d2dfd841ecd87306778b
Jiajun Wang <jwang@stumbleupon.com>
2014-08-12 12:30:15 -0700

    SubstringWithConcatenationOfAllWords.java

--- src/InterviewCode/SubstringWithConcatenationOfAllWords.java
+++ src/InterviewCode/SubstringWithConcatenationOfAllWords.java
@@ -65,14 +65,4 @@ public class SubstringWithConcatenationOfAllWords
 
         return result;
     }
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        // TODO Auto-generated method stub
-
-    }
-
 }
